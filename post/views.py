8d2346ec85657ef7fa8ac40b5f66a9e330208489
BAUIRJANDeV <mamatalivbauirjan@email.com>
2025-07-13 06:57:16 +0500

    blok sayt1

--- post/views.py
+++ post/views.py
@@ -4,5 +4,14 @@ from .models import *
 
 def index(request):
     categories=Categorya.objects.all()
-    return render(request,'index.html',{'categories':categories})
+    first_news=[]
+    for category in categories:
+        category_first_post=News.objects.filter(category=category).first()
+        if category_first_post is not None:
+            first_news.append(category_first_post)
+    if len(first_news)<4:
+        news=News.objects.all()
+        first_news.extend(news[len(news)-4:len(news)-len(first_news)])
+    print(first_news)
+    return render(request,'index.html',{'categories':categories,'first_news':first_news})
 # Create your views here.
