4b67ed4c8d9d964d7b2cecaacfcb2180cac32ca9
BAUIRJANDeV <mamatalivbauirjan@email.com>
2025-07-14 18:38:31 +0500

    blok site

--- post/views.py
+++ post/views.py
@@ -12,6 +12,8 @@ def index(request):
     if len(first_news)<4:
         news=News.objects.all()
         first_news.extend(news[len(news)-4:len(news)-len(first_news)])
-    print(first_news)
-    return render(request,'index.html',{'categories':categories,'first_news':first_news})
+    news=News.objects.all().order_by('-created_at')
+    news1 = News.objects.filter(category__name='Jahon').order_by('-created_at')
+    news2 =  News.objects.filter(category__name='O‘zbekiston').order_by('-created_at')
+    return render(request,'index.html',{'categories':categories,'first_news':first_news,'news':news,'news1':news1,'news2':news2})
 # Create your views here.
